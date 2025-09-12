24cc2552727f05f1e86efefe8c75d17181d2283a
frontEndIcc <asaeidabadi243@gmail.com>
2025-09-12 17:04:01 +0100

    Add logo image to Navbar and update assets
    Navbar now displays a logo using next/image. Updated myLogo.png and favicon.ico assets to new versions.

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
-import  LanguageSwitcher  from "./LanguageSwitcher";
+import LanguageSwitcher from "./LanguageSwitcher";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className="w-full flex justify-between items-center py-6 px-8 text-[var(--primary)] dark:text-[var(--primary)] dark:bg-[var(--background)] bg-[var(--background)] ">
       {/* {logo section} */}
       <div>
-        <Link href="/" className="text-2xl font-bold hover:underline">
+        <Link
+          href="/"
+          className="text-2xl font-bold hover:underline flex gap-2 items-center"
+        >
+          <Image
+            alt="Abolfazl Saeidabadi"
+            src="/images/MyLogo.png"
+            width={40}
+            height={40}
+          />
           Wellcome
         </Link>
       </div>
