89453e364900e76f74a3b3b96457a0b9a03f09ad
frontEndIcc <asaeidabadi243@gmail.com>
2025-09-13 15:03:07 +0100

    update logo

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -11,6 +11,11 @@ const Navbar = () => {
           href="/"
           className="text-2xl font-bold hover:underline flex gap-2 items-center"
         >
+          <img
+            src="/images/MyLogo.png"
+            alt="abolfazl saeidabadi"
+            className="w-10 h-10 lg:w-15 lg:h-15 xl:w-20 xl:h-20"
+          />
           Wellcome
         </Link>
       </div>
