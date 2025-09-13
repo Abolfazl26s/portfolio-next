4b3cb141b51df6d218d880b89b8fd26a7d8dd5fc
frontEndIcc <asaeidabadi243@gmail.com>
2025-09-13 14:02:24 +0100

    update MyLogo address

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig = {
         port: "",
         pathname: "/project_data/images/**", // این بخش برای امنیت بیشتر است
       },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      }, // این را هم اضافه کنید
     ],
   },
 };
