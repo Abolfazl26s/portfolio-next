ec83e63cb176aca592cf8e6eba386e99331b2f83
frontEndIcc <asaeidabadi243@gmail.com>
2025-11-29 20:09:09 +0000

    Replace img with Next.js Image in Navbar
    Updated the Navbar component to use the Next.js Image component instead of a standard img tag for the logo, improving image optimization. Also fixed a typo in the welcome text.

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import Image from "next/image";
-
 const Navbar = () => {
   return (
     <nav className="w-full flex justify-between items-center py-6 px-8 text-[var(--primary)] dark:text-[var(--primary)] dark:bg-[var(--background)] bg-[var(--background)] ">
@@ -11,12 +10,16 @@ const Navbar = () => {
           href="/"
           className="text-2xl font-bold hover:underline flex gap-2 items-center"
         >
-          <img
+          
+          <Image
             src="/images/logo.png"
             alt="abolfazl saeidabadi"
+            width={80}
+            height={80}
             className="w-10 h-10 lg:w-15 lg:h-15 xl:w-20 xl:h-20"
+            priority
           />
-          Wellcome
+          Welcome
         </Link>
       </div>
       {/* buttons section */}
